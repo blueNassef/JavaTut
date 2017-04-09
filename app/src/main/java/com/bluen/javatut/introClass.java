0e387c40846797d8f684197ad989c3535edeb30a
root <trois4ever@gmail.com>
2017-04-09 11:20:54 +0200

    first

---
+++ app/src/main/java/com/bluen/javatut/introClass.java
@@ -0,0 +1,34 @@
+package com.bluen.javatut;
+
+import android.app.Fragment;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.text.method.ScrollingMovementMethod;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+/**
+ * Created by root on 4/7/17.
+ */
+
+public class introClass extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View v= inflater.inflate(R.layout.intro_frag,container,false);
+        Button introVideo= (Button)v.findViewById(R.id.introVideo);
+        introVideo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i= new Intent(Intent.ACTION_VIEW, Uri.parse(""));
+                startActivity(i);
+            }
+        });
+        return v;
+    }
+}
